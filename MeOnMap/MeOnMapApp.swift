5035b8dbf59904bd03929ef08b64e595e51c8c55
Ege <egeege95@gmail.com>
2022-08-30 14:31:57 +0300

    Initial Commit

---
+++ MeOnMap/MeOnMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeOnMapApp.swift
+//  MeOnMap
+//
+//  Created by Ege Çam on 30.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct MeOnMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
